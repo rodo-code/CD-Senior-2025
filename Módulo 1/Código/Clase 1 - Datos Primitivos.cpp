d9e8d52867ca12901f3de2b6f796423ab5b65578
rodo-code <rodolfo.catunta.uturunco@gmail.com>
2025-02-21 18:47:22 -0400

    21 Feb - Ejemplo datos primitivos

---
+++ Módulo 1/Código/Clase 1 - Datos Primitivos.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    bool res=5000; // Cualquier valor que no sea 0 o false, sera tomado como true o 1
+    cout<<res<<endl;
+    return 0;
+}  
